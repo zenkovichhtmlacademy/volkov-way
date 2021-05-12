1a7dd21f2b063c222c2cd3a750aaa770ee0d46c5
Volkov Gennadiy <VolkovGennadiy@users.noreply.github.com>
2021-05-13 00:45:26 +0300

    comments

--- build/js/mobile-menu.js
+++ build/js/mobile-menu.js
@@ -2,6 +2,6 @@ const buttonMenu = document.querySelector('.navigation__button');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 buttonMenu.onclick = () => {
-  mobileMenu.classList.toggle('page-header--none');
   mobileMenu.classList.toggle('page-header--active');
+  mobileMenu.classList.toggle('page-header--none');
 }
