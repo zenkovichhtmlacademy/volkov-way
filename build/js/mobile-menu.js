bcc00d1a48947dcec0acca54fb861595d2895caf
VolkovGennadiy <70528241+VolkovGennadiy@users.noreply.github.com>
2021-05-12 22:54:43 +0300

    Delete build directory

--- build/js/mobile-menu.js
+++
@@ -1,7 +0,0 @@
-const buttonMenu = document.querySelector('.navigation__button');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-buttonMenu.onclick = () => {
-  mobileMenu.classList.toggle('page-header--active');
-  mobileMenu.classList.toggle('page-header--none');
-}
